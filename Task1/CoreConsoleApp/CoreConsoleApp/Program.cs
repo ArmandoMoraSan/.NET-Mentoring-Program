697abee487785a5199cc28c786f619916530cf52
Armando Mora <ingmora.94@gmail.com>
2024-04-30 02:30:35 +0200

    Task 1 & 2

---
+++ Task1/CoreConsoleApp/CoreConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Task2;
+
+namespace CoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your username: ");
+            string username = Console.ReadLine();
+            Console.WriteLine(CurrentTimeUser.SetCurrentTimeUser(username));
+        }
+    }
+}
---
+++ Task1/Task2/Task2/CurrentTimeUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task2
+{
+    public static class CurrentTimeUser
+    {
+        public static string SetCurrentTimeUser(string username)
+        {
+            return $"{DateTime.Now:HH:mm:ss} Hello, {username}!";
+        }
+    }
+}
