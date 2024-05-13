1266662d6ec04dae1a684dc8fdeea4800ebeb582
Armando Mora <ingmora.94@gmail.com>
2024-05-14 01:23:06 +0200

    File System Visitor

---
+++ FileSystemVisitor/FileSystemVisitor/FileSystemVisitor/FileSystemVisitor.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileSystemVisitor
+{
+    public class FileSystemVisitor
+    {
+        public event EventHandler Start;
+        public event EventHandler Finish;
+        public event EventHandler<string> FileFound;
+        public event EventHandler<string> DirectoryFound;
+        public event EventHandler<string> FilteredFileFound;
+        public event EventHandler<string> FilteredDirectoryFound;
+
+        private readonly Func<string, bool> _filter;
+
+        public FileSystemVisitor() : this(null) { }
+
+        public FileSystemVisitor(Func<string, bool> filter)
+        {
+            _filter = filter;
+        }
+
+        public IEnumerable<string> Traverse(string path)
+        {
+            foreach (var directory in Directory.EnumerateDirectories(path))
+            {
+                if (_filter == null || _filter(directory))
+                    yield return directory;
+
+                foreach (var file in Traverse(directory))
+                    yield return file;
+            }
+
+            foreach (var file in Directory.EnumerateFiles(path))
+            {
+                if (_filter == null || _filter(file))
+                    yield return file;
+            }
+        }
+
+        public IEnumerable<string> TraverseWithEvents(string path)
+        {
+            Start?.Invoke(this, EventArgs.Empty);
+
+            foreach (var directory in Directory.EnumerateDirectories(path))
+            {
+                DirectoryFound?.Invoke(this, directory);
+
+                if (_filter == null || _filter(directory))
+                {
+                    FilteredDirectoryFound?.Invoke(this, directory);
+                    yield return directory;
+                }
+
+                foreach (var file in TraverseWithEvents(directory))
+                    yield return file;
+            }
+
+            foreach (var file in Directory.EnumerateFiles(path))
+            {
+                FileFound?.Invoke(this, file);
+
+                if (_filter == null || _filter(file))
+                {
+                    FilteredFileFound?.Invoke(this, file);
+                    yield return file;
+                }
+            }
+            Finish?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+}
---
+++ FileSystemVisitor/FileSystemVisitor/FileSystemVisitor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemVisitor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var visitor = new FileSystemVisitor(file => true);
+
+            visitor.Start += (sender, e) => Console.WriteLine("Starting directory traversal.");
+            visitor.Finish += (sender, e) => Console.WriteLine("Finished directory traversal.");
+            visitor.FileFound += (sender, file) => Console.WriteLine($"File found: {file}");
+            visitor.DirectoryFound += (sender, dir) => Console.WriteLine($"Directory found: {dir}");
+            visitor.FilteredFileFound += (sender, file) => Console.WriteLine($"Filtered file found: {file}");
+            visitor.FilteredDirectoryFound += (sender, dir) => Console.WriteLine($"Filtered directory found: {dir}");
+
+            string path = @"C:\Users\Armando_Mora\Git-NET\.NET-Mentoring-Program\FileSystemVisitor";
+            foreach (var item in visitor.TraverseWithEvents(path)) { }
+        }
+    }
+}
